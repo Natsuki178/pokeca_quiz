b6fa1ae166bedbe90b2e62b81f60409a2e1f044b
hiramatsu <hiramatsu@hstec.jp>
2025-06-05 17:34:18 +0900

    fix デバッグ出力

--- apps/number_of_sheet/views.py
+++ apps/number_of_sheet/views.py
@@ -1,3 +1,5 @@
+import traceback
+
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
@@ -18,19 +20,16 @@ def question_image(request):
     """
     Returns the question image as a PNG response.
     """
-    print("Generating question image")
     deck_code = request.GET.get("deck_code")
-    print(f"Deck code: {deck_code}")
     if not deck_code:
         return Http404("Deck code is required")
 
     try:
         deck = scrape_deck(deck_code)
-        print(f"Deck scraped: {deck.name} ({deck.code})")
         question_image = generate_card_list_image(deck.cards, with_image=False)
         response = HttpResponse(content_type="image/png")
         question_image.save(response, format="PNG")
         return response
-    except ValueError as e:
-        print(e)
+    except Exception as e:
+        traceback.print_exc()
         return HttpResponse(str(e), status=400)
